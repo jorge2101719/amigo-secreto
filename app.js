00cc8d8474edbd4cd9737cb62d87efb1d8585f3e
jorge2101719 <rusosky01@gmail.com>
2025-02-19 15:53:03 -0300

    se corrige la función agregarAmigo, para que no active alerta al carga la página

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaDeAmigos = [];
+let btnAgregar = document.getElementById('amigo');
+let mensajeDeAlerta = 'Debe ingresar un nombre';
+
+btnAgregar.addEventListener('click', agregarAmigo())
 
 //console.log(miAmigo);
 
@@ -7,11 +11,21 @@ let listaDeAmigos = [];
 
 function agregarAmigo () {
     let miAmigo = document.querySelector('#amigo').value;
+
+    if (miAmigo != '') {
         console.log(miAmigo);
         listaDeAmigos.push(miAmigo);
         console.log(listaDeAmigos);
         actualizarListado(miAmigo);
         limpiarCaja();
+    } else {
+        if (miAmigo === '' && listaDeAmigos.length === 0) {
+            // Este es un truco para que el mensaje de alerta no se dispare al cargar la página
+            // Simplemente doy una instrucción vacía, para que nada sea realizado
+        } else {
+            alert(mensajeDeAlerta);
+        }
+    }
 }
 
 function actualizarListado(persona) {
@@ -25,4 +39,4 @@ function limpiarCaja () {
 }
 
 
-agregarAmigo();
+//agregarAmigo();
