48d0b9c4f7158ac06623df1317432825700058e5
jorge2101719 <rusosky01@gmail.com>
2025-02-19 23:10:27 -0300

    modifico ligeramente función sortearAmigo

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaDeAmigos = [];
-let btnAgregar = document.getElementById('amigo');
 let mensajeDeAlerta = 'Debe ingresar un nombre';
-
-btnAgregar.addEventListener('click', agregarAmigo())
-
+let amigosSorteados = [];
+let listado = document.querySelector('#listaAmigos');
 
 // Lógica de la función agregarAmigo
 function agregarAmigo () {
@@ -18,31 +16,28 @@ function agregarAmigo () {
     } else {
         // se impide que el mensaje se muestre al cargar la página
         if (miAmigo === '' && listaDeAmigos.length === 0) {
-            // Este es un truco para que el mensaje de alerta no se dispare al cargar la página
-            // Simplemente doy una instrucción vacía, para que nada sea realizado
+            alert(mensajeDeAlerta);
         } else {
             alert(mensajeDeAlerta);
         }
     }
 }
-
 // Lógica de la función actualizarListado
 function actualizarListado(persona) {
-    let listado = document.querySelector('#listaAmigos');
     listado.innerHTML += `<li>${persona}</li>`;    
 }
 
-// Lógica de la función sortearAmigo
+// Lógica de la función sortearAmigo. Dado las características del juego, asumo que el sorteo se realiza un única vez
+// por lo que no sería necesario preguntar si el nombre ya ha sido sorteado antes.
 function sortearAmigo () {
     if (listaDeAmigos.length > 0) {
-        let listado = document.querySelector('#listaAmigos');
         let resultadoDelSorteo = document.querySelector('#resultado');
         let valorAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
         let amigoSorteado = listaDeAmigos[valorAleatorio];
         listado.innerHTML = '';
         resultadoDelSorteo.innerHTML = `El amigo(a) sorteado(a) es ${amigoSorteado}`
     } else {
-        alert('No hay nombres ingresados');
+        alert('Hasta el momento, no hay nombre ingresados. Por favor, ingrese al menos un nombre');
     }
 }
 
@@ -50,3 +45,5 @@ function sortearAmigo () {
 function limpiarCaja () {
     document.getElementById('amigo').value = '';
 }
+
+//agregarAmigo();
